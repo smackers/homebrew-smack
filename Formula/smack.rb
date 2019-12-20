f7df59f254c4a6d080763d9885a0a63800cb2e2b
Michael Emmi <emmmi@amazon.com>
2019-12-20 13:40:04 -0500

    First homebrew formula for Smack.

---
+++ Formula/smack.rb
@@ -0,0 +1,29 @@
+class Smack < Formula
+  desc "SMACK Software Verifier and Verification Toolchain"
+  homepage "http://smackers.github.io"
+  url "https://github.com/smackers/smack/archive/v2.4.0.tar.gz"
+  sha256 "32ebe2d99044e74e91db611934c781d740529213e6fdcc2b29a930ee14da7b93"
+
+  depends_on "cmake" => :build
+  depends_on "llvm@8"
+  depends_on "python"
+  depends_on "z3"
+
+  def install
+    system "cmake", ".", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_BUILD_TYPE=Debug"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"a.c").write <<~EOS
+      #include <smack.h>
+      int main() {
+        assert(1);
+        return 0;
+      }
+    EOS
+
+    # TODO: remove `-t` once Smack no longer installs Corral
+    system "#{bin}/smack", (testpath/"a.c"), "-t"
+  end
+end
